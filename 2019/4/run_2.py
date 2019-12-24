b9014d94f95d98ce1d686bfbbe2dd4b9150bb119
Чернышев Александр <avc@kodeks.ru>
2019-12-24 13:13:25 +0300

    Path 4

---
+++ 2019/4/run_2.py
@@ -0,0 +1,25 @@
+# https://adventofcode.com/2019/day/4
+
+from contextlib import suppress
+
+count = 0
+
+for password in range(272091, 815432):
+    password = str(password)
+
+    increase = True
+    adjacent = {}
+    for i in range(len(password) - 1):
+        if password[i] > password[i+1]:
+            increase = False
+            continue
+        if password[i] == password[i + 1]:
+            if not adjacent.get(password[i]):
+                adjacent[password[i]] = 2
+            else:
+                adjacent[password[i]] += 1
+    result = [k for k, v in adjacent.items() if v == 2]
+    if increase and result:
+        count += 1
+
+print(count)
