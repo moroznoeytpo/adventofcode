4f798923fefd0e082cc9c66be09a7c459d6c79d9
moroznoeytpo <moroznoeytpo@gmail.com>
2020-01-24 10:45:05 +0300

    day 19(2) not done

--- 2015/19/run_2.py
+++ 2015/19/run_2.py
@@ -3,38 +3,22 @@ import re
 
 class Molecules:
     def __init__(self, molecules: dict, starting: str):
-        self.e = molecules.pop('e')
         self.molecules = molecules
         self.starting = starting
-        self.result = set(self.e)
-        self.step = 1
+        self.step = 0
 
-    def run(self):
-        result = set()
-        for line in self.result:
-            length = len(line)
-            for key, value in self.molecules.items():
-                shift = len(key)
-                if key not in line:
-                    result.add(line)
-                    continue
-                i = 0
-                while True:
-                    i = line.find(key, i)
-                    if i < 0:
-                        break
-                    for item in value:
-                        result.add(line[0:i] + item + line[i + shift:length])
-                    i += 1
-        self.result = result
+    def remove(self):
+        for key, value in self.molecules.items():
+            for item in value:
+                if item in self.starting:
+                    self.step += self.starting.count(item)
+                    self.starting = self.starting.replace(item, key)
 
     def find(self):
-        while True:
-            self.run()
-            self.step += 1
+        for i in range(10):
+            self.remove()
+            print(self.starting)
             print(self.step)
-            if self.starting in self.result:
-                break
 
 
 input_file = open('2015/19/input.txt', 'r')
@@ -49,8 +33,5 @@ for line in input_file:
     elif re.search(r"\w+", line):
         starting = line
 
-molecules['e'] = ['H', 'O']
-
 obj = Molecules(molecules, starting)
 obj.find()
-print(obj.step)
